2321625cc1c666f5da7c43969574ab908a981b1b
Nahom Mekonnen <imnahomm@gmail.com>
2024-06-04 07:12:57 -1200

    another changes

--- models/users.js
+++ models/users.js
@@ -7,4 +7,7 @@ const connection = mysql.createConnection({
     database:'HRM'
 })
 
+const cDb = "CREATE DATABASE hrm "
+const cTable = "CREATE TABLE users (id int(11) PRIMARY KEY AUTOINCREMENT,first_name varchar(255),last_name varchar(255),email varchar(255),pwd varchar(255),profile_pic varchar(255))"
+
 module.exports = connection;
