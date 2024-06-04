d7e3de56ddf16c6d2606172ee8122e0e38d88888
Nahom Mekonnen <imnahomm@gmail.com>
2024-06-03 15:23:29 -1200

    Second Commit

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,12 @@ const { v4: uuidv4 } = require('uuid')
 const path = require('path')
 const con = require('../models/users');
 const { error } = require('console');
+var session = require('express-session')
+router.use(session({
+    secret: "1234567890",
+    resave: false,
+    saveUninitialized: false
+}))
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -56,11 +62,11 @@ router.post('/signedup', upload, async (req, res) => {
             console.error('Error querying database:', err);
             return result.render('signup', { mess: 'An error occurred while processing your request' });
         }
-    
+
         // Check if a user with the email already exists
         if (result.length > 0) {
             // User with the email already exists
-             res.render('signup', { mess:`Email ${result[0].email} already exists,Please try another one` });
+            res.render('signup', { mess: `Email ${result[0].email} already exists,Please try another one` });
         } else {
             con.query('INSERT INTO users(first_name,last_name,email,pwd,profile_pic) values(?,?,?,?,?)', [first_name, last_name, email, hashedpwd, req.file.filename], async (error, results, fields) => {
                 if (error) {
@@ -71,12 +77,50 @@ router.post('/signedup', upload, async (req, res) => {
             })
         }
     });
-    
-    
+
+
+})
+
+router.get('/signin', (req, res) => {
+    res.render('signin', { messin: null })
+})
+
+router.post('/signedin', (req, res) => {
+    const { email, pwd } = req.body
+
+    if (!pwd || !email) {
+        res.render('signin', { messin: 'Please Fill in all the feilds' });
+    }
+
+    con.connect(function (err) {
+        (err) ? console.log(err) : console.log('Connected Successfully')
+    });
+
+    con.query('SELECT pwd from users where email = ?', [email], async (err, response) => {
+        const pwdDb = await response[0].pwd
+        const pwdMatch = await bcrypt.compare(pwd, pwdDb)
+
+        if (pwdMatch) {
+            req.session.userId = email;
+            res.redirect('/index')
+        } else {
+            res.render('signin', { messin: 'Password Dont Match' });
+        }
+    })
+})
+
+router.get('/',(req,res)=>{
+    res.render('index',{messind:null})
+})
+
+router.get('/index',(req,res)=>{
+    const user = req.session.userId
+    res.render('index',{messind:null})
 })
 
 process.on('exit', () => {
     connection.end();
 });
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 const router = require('./routes/router');
+// Session middleware setup
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:false}))
